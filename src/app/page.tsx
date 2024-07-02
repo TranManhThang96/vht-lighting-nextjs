f16f28a037cecc6836f07b6b4af17594485bb74d
ThangTm <tranmanhthang96@gmail.com>
2024-07-02 10:08:43 +0700

    feat: Update banner images and add new decoration images

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -93,6 +93,25 @@ export default function Home() {
       </section>
 
       <section className="relative min-h-[700px] bg-gradient-to-b from-blue-100 to-[#f7f7f7] before:absolute before:left-0 before:right-0 before:top-[-134px] before:h-[134px] before:bg-decorate before:bg-[length:100%_134px] before:bg-left-top before:bg-no-repeat before:content-['']">
+        <div className='mx-auto w-full xl:max-w-screen-xl 2xl:max-w-screen-2xl'>
+          <h3 className='mt-10 text-center text-base font-semibold uppercase text-blue-200'>Bộ sưu tập</h3>
+          <h2 className='mt-4 text-center text-[42px] font-semibold leading-[50px] text-gray-100'>
+            Tổng hợp các hình ảnh thực tế <br /> khách hàng nâng cấp đèn tại VHT Lighting
+          </h2>
+        </div>
+      </section>
+
+      <section className='min-h-[900px]'>
+        <div className='mx-auto w-full py-10 xl:max-w-screen-xl 2xl:max-w-screen-2xl'>
+          <h3 className='mt-10 text-center text-base font-semibold uppercase text-blue-200'>Sản phẩm</h3>
+          <h2 className='mt-4 text-center text-[42px] font-semibold leading-[50px] text-gray-100'>
+            Cùng khám phá các sản phẩm <br />
+            vô cùng chất lượng mà VHT Lighting cung cấp
+          </h2>
+        </div>
+      </section>
+
+      <section className="relative min-h-[700px] bg-gradient-to-b from-blue-100 to-[#f7f7f7] before:absolute before:left-0 before:right-0 before:top-[-134px] before:h-[134px] before:bg-decorate before:bg-[length:100%_134px] before:bg-left-top before:bg-no-repeat before:content-['']">
         <div className='mx-auto flex w-full gap-10 xl:max-w-screen-xl 2xl:max-w-screen-2xl'>
           <div className=''>
             <Image src='https://picsum.photos/720/550' width={720} height={550}></Image>
